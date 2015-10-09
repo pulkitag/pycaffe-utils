7aed95883e18936de72ce20d08ffe1214015ffd6
pulkitag <pulkitag@berkeley.edu>
2015-10-09 15:23:21 -0700

    Added Quaternion computations

--- pycaffe_config.py
+++ pycaffe_config.py
@@ -10,7 +10,9 @@ cfg.HOSTNAME = socket.gethostname()
 if cfg.HOSTNAME in ['anakin', 'vader', 'spock', 'poseidon']:
 	cfg.IS_EC2 = False	
 	cfg.CAFFE_PATH = '/work4/pulkitag-code/pkgs/caffe-v2-3'
+	cfg.STREETVIEW_CODE_PATH = '/work4/pulkitag-code/code/projStreetView'
 else:
 	cfg.IS_EC2 = True
 	cfg.CAFFE_PATH = '/home/ubuntu/caffe-v2-3'
+	cfg.STREETVIEW_CODE_PATH = '/home/ubuntu/code/streetview'
 	
