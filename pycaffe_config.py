655390ce50f00c57a6a8c85a5d18f500c06c72bf
pulkitag <pulkitag@berkeley.edu>
2015-10-14 04:02:21 -0700

    Changes to pycaffe_config to account for nvidia account

--- pycaffe_config.py
+++ pycaffe_config.py
@@ -4,6 +4,7 @@
 
 import socket
 from easydict import EasyDict as edict
+from os import path as osp
 
 cfg = edict()
 cfg.HOSTNAME = socket.gethostname()
@@ -11,8 +12,12 @@ if cfg.HOSTNAME in ['anakin', 'vader', 'spock', 'poseidon']:
 	cfg.IS_EC2 = False	
 	cfg.CAFFE_PATH = '/work4/pulkitag-code/pkgs/caffe-v2-3'
 	cfg.STREETVIEW_CODE_PATH = '/work4/pulkitag-code/code/projStreetView'
+	cfg.STREETVIEW_DATA_MAIN = '/data0'
 else:
 	cfg.IS_EC2 = True
 	cfg.CAFFE_PATH = '/home/ubuntu/caffe-v2-3'
 	cfg.STREETVIEW_CODE_PATH = '/home/ubuntu/code/streetview'
-	
+	if osp.exists('/data0'):
+		cfg.STREETVIEW_DATA_MAIN = '/data0'
+	else:
+		cfg.STREETVIEW_DATA_MAIN = '/puresan/shared'
