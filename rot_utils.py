Fixed rotation utilities for radian/degree confusion for quaternions

rot_utils.py
@@ -12,6 +12,14 @@ from mpl_toolkits.mplot3d import Axes3D
 
 _FLOAT_EPS_4 = np.finfo(float).eps * 4.0
 
+##
+#Convert degrees to radians
+def deg2rad(dg):
+	dg = np.mod(dg, 360)
+	if dg > 180:
+		dg = -(360 - dg)
+	return ((np.pi)/180.) * dg
+
 def get_rot_angle(view1, view2):
 	try:
 		viewDiff = linalg.logm(np.dot(view2, np.transpose(view1)))
@@ -369,52 +377,57 @@ def mat2euler(M, cy_thresh=None, seq='zyx'):
 	return z, y, x
 
 
-def euler2quat(z=0, y=0, x=0):
-    ''' Return quaternion corresponding to these Euler angles
+def euler2quat(z=0, y=0, x=0, isRadian=True):
+	''' Return quaternion corresponding to these Euler angles
 
-    Uses the z, then y, then x convention above
+	Uses the z, then y, then x convention above
 
-    Parameters
-    ----------
-    z : scalar
-       Rotation angle in radians around z-axis (performed first)
-    y : scalar
-       Rotation angle in radians around y-axis
-    x : scalar
-       Rotation angle in radians around x-axis (performed last)
+	Parameters
+	----------
+	z : scalar
+		 Rotation angle in radians around z-axis (performed first)
+	y : scalar
+		 Rotation angle in radians around y-axis
+	x : scalar
+		 Rotation angle in radians around x-axis (performed last)
 
-    Returns
-    -------
-    quat : array shape (4,)
-       Quaternion in w, x, y z (real, then vector) format
+	Returns
+	-------
+	quat : array shape (4,)
+		 Quaternion in w, x, y z (real, then vector) format
 
-    Notes
-    -----
-    We can derive this formula in Sympy using:
+	Notes
+	-----
+	We can derive this formula in Sympy using:
 
-    1. Formula giving quaternion corresponding to rotation of theta radians
-       about arbitrary axis:
-       http://mathworld.wolfram.com/EulerParameters.html
-    2. Generated formulae from 1.) for quaternions corresponding to
-       theta radians rotations about ``x, y, z`` axes
-    3. Apply quaternion multiplication formula -
-       http://en.wikipedia.org/wiki/Quaternions#Hamilton_product - to
-       formulae from 2.) to give formula for combined rotations.
-    '''
-    z = z/2.0
-    y = y/2.0
-    x = x/2.0
-    cz = math.cos(z)
-    sz = math.sin(z)
-    cy = math.cos(y)
-    sy = math.sin(y)
-    cx = math.cos(x)
-    sx = math.sin(x)
-    return np.array([
-             cx*cy*cz - sx*sy*sz,
-             cx*sy*sz + cy*cz*sx,
-             cx*cz*sy - sx*cy*sz,
-             cx*cy*sz + sx*cz*sy])
+	1. Formula giving quaternion corresponding to rotation of theta radians
+		 about arbitrary axis:
+		 http://mathworld.wolfram.com/EulerParameters.html
+	2. Generated formulae from 1.) for quaternions corresponding to
+		 theta radians rotations about ``x, y, z`` axes
+	3. Apply quaternion multiplication formula -
+		 http://en.wikipedia.org/wiki/Quaternions#Hamilton_product - to
+		 formulae from 2.) to give formula for combined rotations.
+	'''
+  
+	if not isRadian:
+		z = ((np.pi)/180.) * z
+		y = ((np.pi)/180.) * y
+		x = ((np.pi)/180.) * x
+	z = z/2.0
+	y = y/2.0
+	x = x/2.0
+	cz = math.cos(z)
+	sz = math.sin(z)
+	cy = math.cos(y)
+	sy = math.sin(y)
+	cx = math.cos(x)
+	sx = math.sin(x)
+	return np.array([
+					 cx*cy*cz - sx*sy*sz,
+					 cx*sy*sz + cy*cz*sx,
+					 cx*cz*sy - sx*cy*sz,
+					 cx*cy*sz + sx*cz*sy])
 
 
 def quat2euler(q):

vis_utils.py
@@ -15,7 +15,7 @@ TMP_DATA_DIR = '/data1/pulkitag/others/caffe_tmp_data/'
 
 ##
 # Plots pairs of images. 
-def plot_pairs(im1, im2, fig=None, titleStr=''):
+def plot_pairs(im1, im2, fig=None, titleStr='', figTitle=''):
 	if fig is None:
 		fig = plt.figure()
 	plt.figure(fig.number)
@@ -26,6 +26,7 @@ def plot_pairs(im1, im2, fig=None, titleStr=''):
 	ax2.imshow(im2.astype(np.uint8))
 	ax2.axis('off')
 	plt.title(titleStr)
+	fig.suptitle(figTitle)
 	plt.show()
 
 ##